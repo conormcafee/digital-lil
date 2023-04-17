c312174d54ac00ad93ec0548d9d26e7d348511ad
Conor McAfee <hello@conormcafee.co.uk>
2023-04-17 22:18:12 +0100

    WIP Homepage

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
-
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Input Directory : src Output directory: _site
 
-  eleventyConfig.addPassthroughCopy("assets")
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("assets/**/*.jpg");
+  eleventyConfig.addPassthroughCopy("assets/**/*.jpeg");
 
   return {
-      dir: { input: 'src', output: '_site' }
+    dir: { input: "src", output: "_site" },
   };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['_site/**/*.html'],
+  content: ["_site/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
-        figtree: ['Figtree', 'sans-serif']
+        figtree: ["Figtree", "sans-serif"],
       },
       colors: {
-        'secondary': {
-          'light': '#CCCDCE',
-          DEFAULT: '#142745',
+        secondary: {
+          light: "#CCCDCE",
+          DEFAULT: "#142745",
         },
-        'accent': {
-          'light': '#A2E0D1',
-          DEFAULT: '#46C1A4',
-        }
-      }
-    }
+        accent: {
+          light: "#A2E0D1",
+          DEFAULT: "#46C1A4",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
